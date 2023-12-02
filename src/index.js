0acffb9e385c929fccc01bcc0f5a58879c236e0c
kam-ilab <146891307+kam-ilab@users.noreply.github.com>
2023-12-03 01:03:34 +0200

    Added basic search engine with no API

---
+++ src/index.js
@@ -0,0 +1,9 @@
+function executeSearchSubmission(event) {
+  event.preventDefault();
+  let searchInput = document.querySelector("#search-input");
+  let cityElement = document.querySelector("#weather-city");
+  cityElement.innerHTML = searchInput.value;
+}
+
+let searchFormElement = document.querySelector("#search-form");
+searchFormElement.addEventListener("submit", executeSearchSubmission);
